2e4d100d15a0465b181853200698fb6f56562a90
Antoine Corcy <dev@sbin.dk>
2014-07-30 23:43:37 +0200

    add IpInfoProvider + tests

---
+++ src/Geocoder/Provider/IpInfoProvider.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider;
+
+use Geocoder\Exception\UnsupportedException;
+use Geocoder\Exception\NoResultException;
+
+/**
+ * @author Antoine Corcy <contact@sbin.dk>
+ */
+class IpInfoProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * @var string
+     */
+    const ENDPOINT_URL = 'http://ipinfo.io/%s/json';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getGeocodedData($address)
+    {
+        if (!filter_var($address, FILTER_VALIDATE_IP)) {
+            throw new UnsupportedException('The IpInfoProvider does not support Street addresses.');
+        }
+
+        if (in_array($address, array('127.0.0.1', '::1'))) {
+            return array($this->getLocalhostDefaults());
+        }
+
+        $query   = sprintf(self::ENDPOINT_URL, $address);
+        $content = $this->getAdapter()->getContent($query);
+        $data    = json_decode($content, true);
+
+        if (empty($data) || '' === $data['loc']) {
+            throw new NoResultException(sprintf('Could not execute query %s', $query));
+        }
+
+        $location = explode(',', $data['loc']);
+
+        return array(array_merge($this->getDefaults(), array(
+            'latitude'    => $location[0],
+            'longitude'   => $location[1],
+            'city'        => isset($data['city'])    ? $data['city']    : null,
+            'zipcode'     => isset($data['postal'])  ? $data['postal']  : null,
+            'region'      => isset($data['region'])  ? $data['region']  : null,
+            'countryCode' => isset($data['country']) ? $data['country'] : null,
+        )));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getReversedData(array $coordinates)
+    {
+        throw new UnsupportedException('The IpInfoProvider is not able to do reverse geocoding.');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'ip_info';
+    }
+}
---
+++ tests/Geocoder/Tests/Provider/IpInfoProviderTest.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Geocoder\Tests\Provider;
+
+use Geocoder\Tests\TestCase;
+use Geocoder\Provider\IpInfoProvider;
+
+class IpInfoProviderTest extends TestCase
+{
+    public function testGetName()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $this->assertEquals('ip_info', $provider->getName());
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     * @expectedExceptionMessage The IpInfoProvider does not support Street addresses.
+     */
+    public function testGetGeocodedDataWithNull()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $provider->getGeocodedData(null);
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     * @expectedExceptionMessage The IpInfoProvider does not support Street addresses.
+     */
+    public function testGetGeocodedDataWithEmpty()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $provider->getGeocodedData('');
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     * @expectedExceptionMessage The IpInfoProvider does not support Street addresses.
+     */
+    public function testGetGeocodedDataWithAddress()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $provider->getGeocodedData('10 avenue Gambetta, Paris, France');
+    }
+
+    public function testGetGeocodedDataWithLocalhostIPv4()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $result = $provider->getGeocodedData('127.0.0.1');
+
+        $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+
+        $result = $result[0];
+        $this->assertInternalType('array', $result);
+        $this->assertArrayNotHasKey('latitude', $result);
+        $this->assertArrayNotHasKey('longitude', $result);
+        $this->assertArrayNotHasKey('zipcode', $result);
+        $this->assertArrayNotHasKey('timezone', $result);
+
+        $this->assertEquals('localhost', $result['city']);
+        $this->assertEquals('localhost', $result['region']);
+        $this->assertEquals('localhost', $result['county']);
+        $this->assertEquals('localhost', $result['country']);
+    }
+
+    public function testGetGeocodedDataWithLocalhostIPv6()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $result = $provider->getGeocodedData('::1');
+
+        $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+
+        $result = $result[0];
+        $this->assertInternalType('array', $result);
+        $this->assertArrayNotHasKey('latitude', $result);
+        $this->assertArrayNotHasKey('longitude', $result);
+        $this->assertArrayNotHasKey('zipcode', $result);
+        $this->assertArrayNotHasKey('timezone', $result);
+
+        $this->assertEquals('localhost', $result['city']);
+        $this->assertEquals('localhost', $result['region']);
+        $this->assertEquals('localhost', $result['county']);
+        $this->assertEquals('localhost', $result['country']);
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\NoResultException
+     * @expectedExceptionMessage Could not execute query http://ipinfo.io/74.200.247.59/json
+     */
+    public function testGetGeocodedDataWithRealIPv4GetsNullContent()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapterReturns(null));
+        $provider->getGeocodedData('74.200.247.59');
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\NoResultException
+     * @expectedExceptionMessage Could not execute query http://ipinfo.io/74.200.247.59/json
+     */
+    public function testGetGeocodedDataWithRealIPv4GetsEmptyContent()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapterReturns(''));
+        $provider->getGeocodedData('74.200.247.59');
+    }
+
+    public function testGetGeocodedDataWithRealIPv4()
+    {
+        $json = <<<JSON
+{
+    "ip": "74.200.247.59",
+    "hostname": "wordpress.com",
+    "city": "Plano",
+    "region": "Texas",
+    "country": "US",
+    "loc": "33.0347,-96.8134",
+    "org": "AS22576 Layered Technologies, Inc.",
+    "postal": "75093"
+}
+JSON;
+
+        $provider = new IpInfoProvider($this->getMockAdapterReturns($json));
+        $result = $provider->getGeocodedData('74.200.247.59');
+
+        $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+
+        $result = $result[0];
+        $this->assertInternalType('array', $result);
+        $this->assertEquals(33.0347, $result['latitude'], '', 0.01);
+        $this->assertEquals(-96.8134, $result['longitude'], '', 0.01);
+        $this->assertEquals(75093, $result['zipcode']);
+        $this->assertEquals('Plano', $result['city']);
+        $this->assertEquals('Texas', $result['region']);
+        $this->assertEquals('US', $result['countryCode']);
+    }
+
+    public function testGetGeocodedDataWithRealIPv6()
+    {
+        $json = <<<JSON
+{
+    "ip": "74.200.247.59",
+    "hostname": "No Hostname",
+    "city": "Plano",
+    "region": "Texas",
+    "country": "US",
+    "loc": "33.0347,-96.8134",
+    "org": "AS22576 Layered Technologies, Inc.",
+    "postal": "75093"
+}
+JSON;
+
+        $provider = new IpInfoProvider($this->getMockAdapterReturns($json));
+        $result = $provider->getGeocodedData('::ffff:74.200.247.59');
+
+        $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+
+        $result = $result[0];
+        $this->assertInternalType('array', $result);
+        $this->assertEquals(33.0347, $result['latitude'], '', 0.01);
+        $this->assertEquals(-96.8134, $result['longitude'], '', 0.01);
+        $this->assertEquals(75093, $result['zipcode']);
+        $this->assertEquals('Plano', $result['city']);
+        $this->assertEquals('Texas', $result['region']);
+        $this->assertEquals('US', $result['countryCode']);
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\NoResultException
+     * @expectedExceptionMessage Could not execute query http://ipinfo.io/255.255.255.255/json
+     */
+    public function testGetGeocodedDataWithoutLocation()
+    {
+        $json = <<<JSON
+{
+    "ip": "255.255.255.255",
+    "hostname": "No Hostname",
+    "loc": "",
+    "bogon": true
+}
+JSON;
+
+        $provider = new IpInfoProvider($this->getMockAdapterReturns($json));
+        $provider->getGeocodedData('255.255.255.255');
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\NoResultException
+     * @expectedExceptionMessage Could not execute query http://ipinfo.io/::ffff:74.200.247.59/json
+     */
+    public function testGetGeocodedDataWithRealIPv6GetsNullContent()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapterReturns(null));
+        $provider->getGeocodedData('::ffff:74.200.247.59');
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     * @expectedExceptionMessage The IpInfoProvider is not able to do reverse geocoding.
+     */
+    public function testGetReverseData()
+    {
+        $provider = new IpInfoProvider($this->getMockAdapter($this->never()));
+        $provider->getReversedData(array(1, 2));
+    }
+}
