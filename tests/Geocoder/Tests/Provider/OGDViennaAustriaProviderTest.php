60ee681bbaa87e4eb058e00a94d06389b9a8c02a
Bernhard Efler <b.efler@susi.at>
2016-07-25 10:17:03 +0200

    add reverse for OGDViennaAustria

--- tests/Geocoder/Tests/Provider/OGDViennaAustriaProviderTest.php
+++ tests/Geocoder/Tests/Provider/OGDViennaAustriaProviderTest.php
@@ -102,13 +102,39 @@ class OGDViennaAustriaProviderTest extends TestCase
         $provider->geocode('::ffff:74.200.247.59');
     }
 
-    /**
-     * @expectedException \Geocoder\Exception\UnsupportedOperation
-     * @expectedExceptionMessage The OGDViennaAustria provider is not able to do reverse geocoding.
-     */
     public function testReverse()
     {
-        $provider = new OGDViennaAustriaProvider($this->getMockAdapter($this->never()));
-        $provider->reverse(1, 2);
+        $provider = new OGDViennaAustriaProvider($this->getAdapter());
+        $result = $provider->reverse(48.230149, 16.350108);
+
+        $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+
+        $result = $result[0];
+        $this->assertInternalType('array', $result);
+        $this->assertEquals(48.230149, $result['latitude'], '', 0.0001);
+        $this->assertEquals(16.350108, $result['longitude'], '', 0.0001);
+        $this->assertArrayHasKey('south', $result['bounds']);
+        $this->assertArrayHasKey('north', $result['bounds']);
+        $this->assertArrayHasKey('east', $result['bounds']);
+        $this->assertArrayHasKey('west', $result['bounds']);
+        $this->assertEquals('Währinger Gürtel', $result['streetName']);
+        $this->assertEquals('121', $result['streetNumber']);
+        $this->assertEquals('1180', $result['zipcode']);
+        $this->assertEquals('Wien', $result['city']);
+        $this->assertEquals('Vienna', $result['region']);
+        $this->assertEquals('Vienna', $result['regionCode']);
+        $this->assertEquals('AT', $result['countryCode']);
+        $this->assertEquals('Europe/Vienna', $result['timezone']);
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\NoResult
+     * @expectedExceptionMessage Result distance to far away
+     */
+    public function testReverseWithInvalidCoordinates()
+    {
+        $provider = new OGDViennaAustriaProvider($this->getAdapter());
+        $provider->reverse(16.35, 48.23);
     }
 }
